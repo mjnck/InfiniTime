9e637064aa93b9842646be83dbbab48f24891898
Riku Isokoski <riksu9000@gmail.com>
2021-07-17 12:09:55 +0300

    Improve failsafe

--- src/components/motor/MotorController.cpp
+++ src/components/motor/MotorController.cpp
@@ -14,12 +14,10 @@ void MotorController::SetDuration(uint8_t motorDuration) {
   if (settingsController.GetVibrationStatus() == Controllers::Settings::Vibration::OFF) {
     return;
   }
-  nrf_gpio_pin_clear(pinMotor);
-  if (xTimerChangePeriod(motorTimer, pdMS_TO_TICKS(motorDuration), 0) == pdFAIL) {
-    StopMotor(nullptr);
-  }
-  if (xTimerStart(motorTimer, 0) == pdFAIL) {
-    StopMotor(nullptr);
+  if (xTimerChangePeriod(motorTimer, pdMS_TO_TICKS(motorDuration), 0) == pdPASS) {
+    if (xTimerStart(motorTimer, 0) == pdPASS) {
+      nrf_gpio_pin_clear(pinMotor);
+    }
   }
 }
 
